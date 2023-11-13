75827c50088821548a7d1c93f59fde5ee77a022c
Manoel Vieira <manoelv8@gmail.com>
2023-11-13 08:51:19 -0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,16 +4,8 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+      <Title>APP</Title>
+      <h1>Site in maintenance</h1>
     </main>
   );
 }
